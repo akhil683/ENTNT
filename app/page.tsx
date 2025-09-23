6bb835faf131d78f79c03268e3040b755841625e
akhil683 <onkar.palsra@gmail.com>
2025-09-23 22:51:54 +0530

    feat: auth layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
+"use client"
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function SignUp() {
-  return (
-    <main>
-      <p>hello World</p>
-      <Link href={"/home"}>
-        <Button>Home</Button>
-      </Link>
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const router =  useRouter()
+  const handleSubmit = () => {
+
+    if (!username || !password) {
+      alert("username and password is required.")
+      return
+}
+    localStorage.setItem("username", username)
+    localStorage.setItem("password", password )
+    if (username === process.env.NEXT_PUBLIC_USERNAME && password === process.env.NEXT_PUBLIC_PASSWORD) {
+      router.replace("/home")
+      alert("Login successfull.")
+    } else {
+      alert("Username or Password is incorrect.")
+    }
+  }
+  return ( 
+    <main className="flex justify-center items-center min-h-screen">
+      <section className="max-w-4xl flex flex-col gap-4 bg-blue-300 p-6 rounded-xl">
+      <h1 className="text-white font-semibold">Login to TalentFlow</h1>
+      <form className="flex flex-col gap-4">
+      <Input value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <Input value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick={handleSubmit}>Login</Button>
+      </form>
+      </section>
     </main>
   );
 }

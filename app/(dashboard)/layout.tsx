e77683cf843659817f357e708bc8c73f43d144e5
akhil683 <onkar.palsra@gmail.com>
2025-09-23 23:42:07 +0530

    feat: complete dummy auth verification

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,21 @@
+"use client"
 import { Sidebar } from "@/components/layout/sidebar";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const username = localStorage.getItem("username")
+    const password = localStorage.getItem("password")
+    if (username !== process.env.NEXT_PUBLIC_USERNAME || password !== process.env.NEXT_PUBLIC_PASSWORD) {
+      redirect("/")
+    }
+  }, [])
   return (
+
     <main className="flex h-screen bg-background">
       <Sidebar />
       {children}
